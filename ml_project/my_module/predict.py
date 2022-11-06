3ce3e17687c446a2e65e12097bc3bad8ad5921db
Dmitrii <dmasny@yandex.ru>
2022-11-06 17:19:22 +0300

    loggers added

--- ml_project/my_module/predict.py
+++ ml_project/my_module/predict.py
@@ -1,25 +1,33 @@
 import click
 import pandas as pd
+import logging
 from my_module.data import read_data
 from my_module.features import create_transformer
 from my_module.models import load_model, predict_model
 from my_module.entities import PredictPipelineParams, read_predict_pipeline_params
 
+logger = logging.getLogger("Train")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
 
 def predict_pipeline(predict_pipeline_params: PredictPipelineParams) -> str:
-    # logger.info(f"start predict pipeline with params {predict_pipeline_params}")
+    logger.info(f"start predict pipeline with params {predict_pipeline_params}")
     data = read_data(predict_pipeline_params.input_data_path)
-    # logger.info(f"data.shape is {data.shape}")
+    logger.info(f"data.shape is {data.shape}")
 
     transformer = create_transformer(predict_pipeline_params.feature_params)
     features = transformer.fit_transform(data)
-    # logger.info(f"train_features.shape is {features.shape}")
+    logger.info(f"train_features.shape is {features.shape}")
 
     model = load_model(predict_pipeline_params.model_path)
 
     predicts = predict_model(features, model)
     pd.DataFrame(predicts).to_csv(predict_pipeline_params.output_data_path, index=False)
-    # logger.info(f"predict saved by {predict_pipeline_params.path_to_output}")
+    logger.info(f"predict saved in {predict_pipeline_params.output_data_path}")
 
     return predict_pipeline_params.output_data_path
 
